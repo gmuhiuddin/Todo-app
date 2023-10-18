4b1c26a75c30e482344275aaf29774ea834a32ea
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-10-18 12:01:32 +0500

    added changes

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 let userTodo = document.getElementById("todo-input")
 let AddIndoBtn = document.getElementById("add-btn");
-let Ul = document.getElementById("ul");
+let listDiv = document.getElementById("list-div");
 AddIndoBtn.addEventListener("click" , addTodo)
+
 
 function addTodo (){
     if(userTodo.value === ""){
         alert("Please enter something in input box")
     }else{
-        let li = document.createElement("li");
-    li.innerText = userTodo.value;
+        let div = document.createElement('div');
+div.className = 'div'
+        let p = document.createElement("p");
+        p.className = 'p'
+    p.innerText = userTodo.value;
+
  let delBtn = document.createElement('span')
 delBtn.innerText = "×"
 delBtn.className = 'delBtn'
 delBtn.addEventListener("click",function () {
-    li.remove()
+    div.remove()
 })
+div.appendChild(p)
+div.appendChild(delBtn)
 
-    li.appendChild(delBtn)
-    Ul.appendChild(li)
-
+    listDiv.appendChild(div)
 
 
     userTodo.value = "";
