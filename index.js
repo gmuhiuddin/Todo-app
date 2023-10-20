63de3866b728c823829f41802d3d901176755a48
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-10-20 16:18:41 +0500

    add todo with local storage

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
 let userTodo = document.getElementById("todo-input")
 let AddIndoBtn = document.getElementById("add-btn");
 let listDiv = document.getElementById("list-div");
-AddIndoBtn.addEventListener("click" , addTodo)
+AddIndoBtn.addEventListener("click" ,addTodoByUser)
 
+var userTodos =  JSON.parse(localStorage.getItem('user todo')) || []
 
-function addTodo (){
-    if(userTodo.value === ""){
-        alert("Please enter something in input box")
-    }else{
-        let div = document.createElement('div');
-div.className = 'div'
-        let p = document.createElement("p");
-        p.className = 'p'
-    p.innerText = userTodo.value;
+userTodos.forEach((value,index) => {
+    addTodoByLs(value)
+    })
 
- let delBtn = document.createElement('span')
-delBtn.innerText = "×"
-delBtn.className = 'delBtn'
-delBtn.addEventListener("click",function () {
-    div.remove()
-})
-div.appendChild(p)
-div.appendChild(delBtn)
-
-    listDiv.appendChild(div)
-
-
-    userTodo.value = "";
-}
+    function addTodoByUser (){
+        userTodos.push(userTodo.value)
+    localStorage.setItem('user todo',JSON.stringify(userTodos))
     
+    addTodoByLs()
+    }
+
+function addTodoByLs (value){
     
+        
+            let div = document.createElement('div');
+            div.className = 'div'
+
+            let p = document.createElement("p");
+            p.className = 'p'
+            p.innerText = value?value:userTodo.value;
+
+            let delBtn = document.createElement('span')
+            delBtn.innerText = "×"
+            delBtn.className = 'delBtn'
+
+            div.appendChild(p)
+        div.appendChild(delBtn)
+
+            delBtn.addEventListener("click",function () {
+                
+                let removeDiv = div.childNodes[0].innerText
+                let removeDivIndex = userTodos.indexOf(removeDiv)
+                // console.log(removeDivIndex)
+                userTodos.splice(removeDivIndex,1)
+                localStorage.setItem("user todo",JSON.stringify(userTodos))
+                div.remove()
+            })
+
+        listDiv.appendChild(div)
+
+        
+    
+        userTodo.value = "";
 }
+
+
